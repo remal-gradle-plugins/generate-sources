1fe8c0ba98dbf69fff98378ccdd72969296e1be1
Semyon Levin <231188+remal@users.noreply.github.com>
2025-01-03 13:00:59 -0800

    Initial commit

---
+++ src/test/java/name/remal/gradle_plugins/template/TemplatePluginTest.java
@@ -0,0 +1,38 @@
+package name.remal.gradle_plugins.template;
+
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.packageNameOf;
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.unwrapGeneratedSubclass;
+import static name.remal.gradle_plugins.toolkit.testkit.ProjectValidations.executeAfterEvaluateActions;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import name.remal.gradle_plugins.toolkit.testkit.TaskValidations;
+import org.gradle.api.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@RequiredArgsConstructor
+class TemplatePluginTest {
+
+    final Project project;
+
+    @BeforeEach
+    void beforeEach() {
+        project.getPluginManager().apply(TemplatePlugin.class);
+    }
+
+    @Test
+    void pluginTasksDoNotHavePropertyProblems() {
+        executeAfterEvaluateActions(project);
+
+        val taskClassNamePrefix = packageNameOf(TemplatePlugin.class) + '.';
+        project.getTasks().stream()
+            .filter(task -> {
+                val taskClass = unwrapGeneratedSubclass(task.getClass());
+                return taskClass.getName().startsWith(taskClassNamePrefix);
+            })
+            .map(TaskValidations::markTaskDependenciesAsSkipped)
+            .forEach(TaskValidations::assertNoTaskPropertiesProblems);
+    }
+
+}
